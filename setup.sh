dccd64f9f6d77f1c9705abdc310343c06093c38c
Retro <rdgc_95@hotmail.es>
2021-11-29 19:20:37 -0700

    Dashboard updates

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@
 echo "Starting setup..."
 echo "> Setting up emacs..."
 echo ">> Copying .emacs"
-cp ./files/.emacs ~/
-cp ./files/settings.org ~/.emacs.d
+mkdir -p ~/.emacs.d
+cp ./files/.emacs ~/.emacs.d/init.el
+cp ./files/settings.org ~/.emacs.d/
 echo "> Setting up bash"
 echo ">> Copying .bashrc"
 cp ./files/.bashrc ~/
-echo ">> Copying .bashrc"
+echo ">> Copying aliasrc"
 cp ./files/aliasrc ~/.config/
-echo ">> Sourcing bash"
+echo ">> Sourcing bash (THIS MAY FAIL)"
 source ~/.bashrc
 echo "> Setting up picom"
 echo ">> Copying picom.conf"
@@ -17,6 +18,12 @@ sudo cp ./files/picom.conf /etc/xdg/
 echo "> Setting up i3"
 echo ">> Copying i3 config"
 cp ./files/i3/config ~/.config/i3/
+echo "> Setting up URxvt"
+echo ">> Copying .Xresources config"
+cp ./files/.Xresources ~/
+echo ">> Updating URxvt (Updates will be visible when oppening a new terminal)"
+xrdb -merge ~/.Xresources
+
 i3-msg reload
 i3-msg restart
 echo "DONE!"
